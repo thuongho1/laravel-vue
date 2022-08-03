8bb0e12826b0621455dbe3f32c68663230d91195
ThuongHo <Thuong.HoThiMy1@Nashtechglobal.com>
2022-08-03 18:08:41 +0700

    Remove unuse

--- vue-front/src/store/services/base-service.js
+++
@@ -1,9 +0,0 @@
-const baseOptions = {
-  headers: {
-    'Accept': 'application/vnd.api+json',
-    'Content-Type': 'application/vnd.api+json',
-  },
-  url: process.env.VUE_APP_API_BASE_URL,
-};
-
-export default baseOptions;
